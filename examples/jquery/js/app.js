732c013c2fd33db1297fe4d14b4ae9a475934db0
Gloria Lee <gmgl21@hotmail.com>
2017-06-08 14:20:06 -0700

    Create app.js
    Removed util.store from render method, created storeTodos method to save the todos.

--- examples/jquery/js/app.js
+++ examples/jquery/js/app.js
@@ -70,7 +70,9 @@ jQuery(function ($) {
 			$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
 			this.renderFooter();
 			$('#new-todo').focus();
-			util.store('todos-jquery', this.todos);
+		},
+		storeTodos: function () {
+		        util.store('todos-jquery', this.todos);
 		},
 		renderFooter: function () {
 			var todoCount = this.todos.length;
@@ -90,7 +92,7 @@ jQuery(function ($) {
 			this.todos.forEach(function (todo) {
 				todo.completed = isChecked;
 			});
-
+                        this.storeTodos();
 			this.render();
 		},
 		getActiveTodos: function () {
@@ -117,6 +119,7 @@ jQuery(function ($) {
 		destroyCompleted: function () {
 			this.todos = this.getActiveTodos();
 			this.filter = 'all';
+			this.storeTodos();
 			this.render();
 		},
 		// accepts an element from inside the `.item` div and
@@ -147,12 +150,13 @@ jQuery(function ($) {
 			});
 
 			$input.val('');
-
+                        this.storeTodos();
 			this.render();
 		},
 		toggle: function (e) {
 			var i = this.indexFromEl(e.target);
 			this.todos[i].completed = !this.todos[i].completed;
+			this.storeTodos();
 			this.render();
 		},
 		edit: function (e) {
@@ -183,11 +187,12 @@ jQuery(function ($) {
 			} else {
 				this.todos[this.indexFromEl(el)].title = val;
 			}
-
+                        this.storeTodos();
 			this.render();
 		},
 		destroy: function (e) {
 			this.todos.splice(this.indexFromEl(e.target), 1);
+			this.storeTodos();
 			this.render();
 		}
 	};
